688d98802c2c035976b50b2d1c2727faccbb0fd0
techundead <marcolasarte@gmail.com>
2018-04-23 16:22:09 +0200

    Mise à Jours Setup SQL

--- Setup/Install_BDD.bat
+++ Setup/Install_BDD.bat
@@ -1,3 +1,4 @@
-sqlcmd -E -S .\SQLEXPRESS -i "%~dp0creat_lapyr.sql"
-sqlcmd -E -S .\SQLEXPRESS -i "%~dp0lapyr.sql"
+sqlcmd -E -S .\SQLEXPRESS -i "%~dp0lib\creat_lapyr.sql"
+sqlcmd -E -S .\SQLEXPRESS -i "%~dp0lib\lapyr.sql"
+sqlcmd -E -S .\SQLEXPRESS -i "%~dp0lib\donnee_lapyr.sql"
 pause
